ee3f170e66a46c386e24df94889e1766a6af7fe4
Malik <malikoabdulaziz@gmail.com>
2021-08-01 11:32:24 +0100

    add redis db

--- src/app.js
+++ src/app.js
@@ -1,40 +1,52 @@
 const express = require("express");
 const qinu = require("qinu");
-const { connect } = require("mongoose");
+const redis = require("redis");
+const client = redis.createClient();
 const cors = require("cors");
 require("dotenv").config();
-const { init } = require("./database");
-const { Url } = require("./models/urlCollection");
+
 const app = express();
 
 app.use(express.urlencoded({ extended: false }));
 app.use(express.json());
 app.use(cors());
-app.get("/:id", async (req, res) => {
-  try {
-    const response = await Url.find({ shortUrl: req.params.id });
-    res.redirect(301, response[0].longUrl);
-  } catch (error) {
-    res.send(error);
-  }
+
+client.on("error", function (error) {
+  console.error(error);
+});
+client.on("connect", () => {
+  console.log("connection to redis db established");
 });
 
-app.post("/", async (req, res) => {
+app.get("/:id", (req, res) => {
+  client.get(req.params.id, (err, reply) => {
+    if (err) {
+      res.json({
+        message: "there was an error prcessing your request, please try again",
+      });
+    } else {
+      res.redirect(301, reply);
+    }
+  });
+});
+
+app.post("/", (req, res) => {
   const { longUrl } = req.body;
-  try {
-    const randomString = qinu({
-      length: 7,
-    });
-    const url = await Url.create({ longUrl, shortUrl: randomString });
-    res.status(200).json({ message: url.shortUrl });
-  } catch (error) {
-    res
-      .status(403)
-      .json({ message: "sorry, there was an error processing your request" });
-  }
+  const randomString = qinu({
+    length: 6,
+  });
+  client.set(randomString, longUrl, (err, reply) => {
+    if (err) {
+      res.status(400).json({
+        message:
+          "sorry, there was an error processing your request, please try again",
+      });
+    } else {
+      res.status(200).json({ message: randomString });
+    }
+  });
 });
 
 app.listen(process.env.PORT || 3005, () => {
   console.log(`app is now listening on port ${process.env.PORT || 3005}`);
-  init();
 });
